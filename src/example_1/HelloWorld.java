cde696d6be26a10fa580a953ae88e4a693373dd5
Mohit Ahuja <mohit.ahuja@accoliteindia.com>
2017-07-10 23:22:51 +0530

    Hello world along with JAutodoc and code coverage done.

---
+++ src/example_1/HelloWorld.java
@@ -0,0 +1,25 @@
+/*
+ * 
+ */
+package example_1;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class HelloWorld.
+ */
+public class HelloWorld {
+	
+	/** The debug. */
+	private static boolean DEBUG = true;
+
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+
+	}
+}
